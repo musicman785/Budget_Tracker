311cb5ee94a1ea4eca87f5e90871426db2d5f810
Chris Salgado <musicman785@gmail.com>
2020-08-03 12:06:50 -0700

    Service worker file finished

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,10 @@
-const STATIC_CACHE = "static-cache-v2";
+const STATIC_CACHE = "static-cache-v1";
+const DYNAMIC_CACHE = "dynamic-cache-v2";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/app.js",
-  "/styleS.css",
+  "/styles.css",
   "/index.js",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "/icons/icon-192x192.png",
@@ -11,40 +12,79 @@ const FILES_TO_CACHE = [
 ];
 //Install service worker
 //=========================================
-self.addEventListener("install", (evt) => {
-  //   console.log("Service worker has been installed!");
+self.addEventListener("install", evt => {
+
   evt.waitUntil(
     caches.open(STATIC_CACHE).then((cache) => {
       console.log("your files were cached!");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-  //self.skipWaiting()
+  self.skipWaiting()
 });
 
 //Activate service worker
 //=========================================
 
-self.addEventListener("activate", (evt) => {
-  //   console.log("Service worker had been activated!");
+self.addEventListener("activate", evt => {
+
   evt.waitUntil(
-    caches.keys().then((keys) => {
+    caches.keys().then(keys => {
       // console.log(keys)
-      return Promise.all(keys.
-        .filter(key => key !== STATIC_CACHE)
-        .map(key => caches.delete(key)))
+      return Promise.all(keys
+        .filter((key) => key !== STATIC_CACHE && key !== DYNAMIC_CACHE)
+        .map((key) => caches.delete(key))
+      );
     })
   );
+  self.clients.claim();
 });
 
 //Fetch request
 //=========================================
 
-self.addEventListener("fetch", (evt) => {
-  //   console.log("Fetch event", evt);
+self.addEventListener("fetch", evt => {
+
+
   evt.respondWith(
-    caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+    caches.match(evt.request.url).then((cacheRes) => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(DYNAMIC_CACHE).then(cache => {
+          cache.put(evt.request, fetchRes.clone());
+          return fetchRes;
+        })
+      });
+    }).catch(err => {
+      return cache.match(evt.request);
     })
   );
 });
+
+// self.addEventListener("fetch", function (evt) {
+//   if (evt.request.url.includes("/api/")) {
+//     evt.respondWith(
+//       caches.open(DYNAMIC_CACHE).then(cache => {
+//         return fetch(evt.request)
+//           .then(response => {
+//             // If the response was good, clone it and store it in the cache.
+//             if (response.status === 200) {
+//               cache.put(evt.request.url, response.clone());
+//             }
+//             return response;
+//           })
+//           .catch(err => {
+//             // Network request failed, try to get it from the cache.
+//             return cache.match(evt.request);
+//           });
+//       }).catch(err => console.log(err))
+//     );
+//     return;
+//   };
+//   evt.respondWith(
+//     caches.open(STATIC_CACHE).then(cache => {
+//       return cache.match(evt.request).then(response => {
+//         return response || fetch(evt.request);
+//       });
+//     })
+//   );
+// })
