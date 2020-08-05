426e68d03f7a78ff23726329567fb90b04a90a21
Chris Salgado <musicman785@gmail.com>
2020-08-05 13:36:31 -0700

    Added heroku enviornment port

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Dependencies 
+//==========================================
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+// Middleware 
+//===============================================
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,14 +18,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
+//==================================
 app.use(require("./routes/api.js"));
 
+//Port listener
+//============================================
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
